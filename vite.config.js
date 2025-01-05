27c997e1f717af1b370dee03f20ad56ccb0b53c5
Siddharam <siddharamsutar23@gmail.com>
2025-01-05 15:55:32 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+ plugins: [react()],
+  server: {
+    port: 3000, // Use PORT from the environment or fallback to 3000
+    host: true, // Allows external access (e.g., Render can bind to it)
+  },})
